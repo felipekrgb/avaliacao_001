e9f9ef391064b6d21462b9840c9a7506ccc648a9
felipekrgb <felipekrgb@gmail.com>
2021-08-16 11:30:21 -0300

    Corrigindo snackbar

--- app/src/main/java/com/example/trabalho001/utils/Extensions.kt
+++ app/src/main/java/com/example/trabalho001/utils/Extensions.kt
@@ -3,15 +3,20 @@ package com.example.trabalho001.utils
 import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.marginBottom
 import androidx.fragment.app.Fragment
 import com.example.trabalho001.R
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.snackbar.Snackbar
 
 
-fun Fragment.snackBar(view: View, @StringRes msgId: Int, @ColorRes colorId: Int) {
+fun AppCompatActivity.snackBar(view: View, @StringRes msgId: Int, @ColorRes colorId: Int) {
     hideKeyboard()
 
     setupSnackbar(view, msgId, colorId).apply {
@@ -19,22 +24,27 @@ fun Fragment.snackBar(view: View, @StringRes msgId: Int, @ColorRes colorId: Int)
     }
 }
 
-fun Fragment.hideKeyboard() {
-    val imm = requireActivity().window.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(requireActivity().window.decorView.windowToken, 0)
+fun AppCompatActivity.hideKeyboard() {
+    val imm = window.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
 }
 
-private fun Fragment.setupSnackbar(
+private fun AppCompatActivity.setupSnackbar(
     v: View,
     @StringRes msgId: Int,
-    @ColorRes colorId: Int
+    @ColorRes colorId: Int,
 ): Snackbar {
     return Snackbar.make(v, msgId, Snackbar.LENGTH_LONG).apply {
-        view.setBackgroundColor(requireActivity().getColor(colorId))
+        view.setBackgroundColor(getColor(colorId))
+
+        anchorView = v.findViewById<NavigationBarView>(R.id.bottomNav)
 
         view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).apply {
-            setTextColor(requireActivity().getColor(R.color.white))
+            setTextColor(getColor(R.color.white))
             textAlignment = View.TEXT_ALIGNMENT_CENTER
         }
+
+        show()
+
     }
 }
